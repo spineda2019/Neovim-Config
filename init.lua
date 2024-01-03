a33b5c2d2300dd32ee00bcfd37c5f9493b660587
= <=>
2024-01-03 14:59:55 -0500

    added quick terminal access with leader+t

--- init.lua
+++ init.lua
@@ -1,7 +1,20 @@
 require("sebastian")
 print("Welcome to Sebastian's NeoVim!")
 
+vim.g.python_host_prog = 'C:\\Users\\SEBASTIANP\\AppData\\Local\\Microsoft\\WindowsApps\\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\\python.exe' 
+
 function ColorMyPencils()
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
+
+-- Quick terminal
+
+function OpenTerminalInNewTab()
+    vim.cmd('lcd %:p:h')
+    vim.cmd('tabnew | terminal')
+    vim.cmd('startinsert')
+end
+
+vim.api.nvim_set_keymap('n', '<leader>t', ':lua OpenTerminalInNewTab()<CR>', {noremap = true, silent = true})
+
