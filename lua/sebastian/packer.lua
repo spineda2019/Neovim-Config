5574719f04157719b1d366b2fac2c4148b065593
= <=>
2024-01-03 07:58:06 -0500

    changed color scheme to vscode

--- lua/sebastian/packer.lua
+++ lua/sebastian/packer.lua
@@ -4,54 +4,57 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
 
-  use ({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
+	-- use ({
+		-- 'rose-pine/neovim',
+		-- as = 'rose-pine',
+		-- config = function()
+			-- vim.cmd('colorscheme rose-pine')
+			-- end
 
-  })
+			-- })
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+			use 'Mofiqul/vscode.nvim'
 
-  use('theprimeagen/harpoon')
 
-  use('mbbill/undotree')
+			use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
-  use ('natecraddock/telescope-zf-native.nvim')
+			use('theprimeagen/harpoon')
 
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v3.x',
-	  requires = {
-		  --- Uncomment these if you want to manage the language servers from neovim
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
+			use('mbbill/undotree')
 
-		  -- LSP Support
-		  {'neovim/nvim-lspconfig'},
-		  -- Autocompletion
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-buffer'},
-		  {'hrsh7th/cmp-path'},
-		  {'saadparwaiz1/cmp_luasnip'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'hrsh7th/cmp-nvim-lua'},
-		  -- Snippets
-		  {'L3MON4D3/LuaSnip'},
-		  {'rafamadriz/friendly-snippets'}
-	  }
-  }
+			use ('natecraddock/telescope-zf-native.nvim')
 
-  end)
+			use {
+				'VonHeikemen/lsp-zero.nvim',
+				branch = 'v3.x',
+				requires = {
+					--- Uncomment these if you want to manage the language servers from neovim
+					{'williamboman/mason.nvim'},
+					{'williamboman/mason-lspconfig.nvim'},
+
+					-- LSP Support
+					{'neovim/nvim-lspconfig'},
+					-- Autocompletion
+					{'hrsh7th/nvim-cmp'},
+					{'hrsh7th/cmp-buffer'},
+					{'hrsh7th/cmp-path'},
+					{'saadparwaiz1/cmp_luasnip'},
+					{'hrsh7th/cmp-nvim-lsp'},
+					{'hrsh7th/cmp-nvim-lua'},
+					-- Snippets
+					{'L3MON4D3/LuaSnip'},
+					{'rafamadriz/friendly-snippets'}
+				}
+			}
+
+		end)
 
