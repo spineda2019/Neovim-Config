4779aeb7153c06b43f81292eca27e1e08331887e
= <=>
2024-01-03 14:59:33 -0500

    added markdown

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -4,7 +4,7 @@ end
 require'nvim-treesitter.configs'.setup {
 
 	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = {"python", "c", "cpp", "rust", "javascript", "typescript", "lua", "vim", "vimdoc", "query" },
+	ensure_installed = {"python", "c", "cpp", "rust", "javascript", "typescript", "lua", "vim", "vimdoc", "query", "markdown"},
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
