417600fdbd11188da11640001bbe67997b520968
= <=>
2024-01-03 15:01:32 -0500

    tabs are now 4 spaces

--- lua/sebastian/set.lua
+++ lua/sebastian/set.lua
@@ -1,4 +1,10 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 10
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
