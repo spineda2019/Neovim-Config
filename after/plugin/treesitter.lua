d2a322616024f78317d49e616cfbd7cbf0e8242a
= <=>
2024-01-03 07:57:10 -0500

    added python

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -4,7 +4,7 @@ end
 require'nvim-treesitter.configs'.setup {
 
 	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = { "c", "cpp", "rust", "javascript", "typescript", "lua", "vim", "vimdoc", "query" },
+	ensure_installed = {"python", "c", "cpp", "rust", "javascript", "typescript", "lua", "vim", "vimdoc", "query" },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
