8a4bc3a64dbcdc0933910dbfa0bd407b2df7d5ef
= <=>
2024-01-05 10:43:08 -0500

    added NeoTree:

--- lua/sebastian/packer.lua
+++ lua/sebastian/packer.lua
@@ -65,4 +65,17 @@ return require("packer").startup(function(use)
 	use({ "echasnovski/mini.nvim", branch = "stable" })
 
 	use("tpope/vim-fugitive")
+
+	use("nvim-tree/nvim-web-devicons")
+
+	use({
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+	})
 end)
