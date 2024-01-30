af0761eac1dd95cbeafabd6c43d647e4264269d6
spineda2019 <spineda@wpi.edu>
2024-01-30 14:10:06 +0000

    specify windows specific settings in if() guard

--- init.lua
+++ init.lua
@@ -1,13 +1,15 @@
 require("sebastian")
 print("Welcome to Sebastian's NeoVim!")
 
-vim.o.shell = "pwsh"
-vim.cmd("set shellcmdflag=-c")
 
 vim.g.netrw_banner = 1
 
+if vim.fn.has('win32') == 1 then
+vim.o.shell = "pwsh"
+vim.cmd("set shellcmdflag=-c")
 vim.g.python_host_prog =
 	"C:\\Users\\SEBASTIANP\\AppData\\Local\\Microsoft\\WindowsApps\\PythonSoftwareFoundation.Python.3.11_qbz5n2kfra8p0\\python.exe"
+end
 
 
 
