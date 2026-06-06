d976fce39bc8b7c0c52feef5ea88421ff9120183
spineda2019 <spineda.wpi.alum@gmail.com>
2026-06-06 18:51:15 -0400

    update

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -47,7 +47,7 @@ return {
 
 	{
 		"nvim-treesitter/nvim-treesitter",
-		tag = "v0.9.3",
+		commit = "4916d6592ede8c07973490d9322f187e07dfefac",
 		build = ":TSUpdate",
         lazy = "true",
         event = "BufReadPost",
@@ -57,7 +57,7 @@ return {
 			else
 				require("nvim-treesitter.install").compilers = { "gcc" }
 			end
-			require("nvim-treesitter.configs").setup({
+			require("nvim-treesitter").install({
 
 				-- A list of parser names, or "all" (the five listed parsers should always be installed)
 				ensure_installed = {
@@ -218,4 +218,27 @@ return {
 			})
 		end,
 	},
+    {
+      "sphamba/smear-cursor.nvim",
+    
+      opts = {
+        -- Smear cursor when switching buffers or windows.
+        smear_between_buffers = true,
+    
+        -- Smear cursor when moving within line or to neighbor lines.
+        -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
+        smear_between_neighbor_lines = true,
+    
+        -- Draw the smear in buffer space instead of screen space when scrolling
+        scroll_buffer_space = true,
+    
+        -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+        -- Smears and particles will look a lot less blocky.
+        legacy_computing_symbols_support = false,
+    
+        -- Smear cursor in insert mode.
+        -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
+        smear_insert_mode = true,
+      },
+    },
 }
